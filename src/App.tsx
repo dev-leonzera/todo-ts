9ebe96d45b4eccdeba3561068877fed343d11526
Leonardo <leonandrade22@gmail.com>
2021-10-05 13:58:23 -0300

    commit 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import {Item} from './types/Item'
+import { ListItem } from './components/ListItem'
+import * as Style from './App.styles'
+import { AddArea } from './components/AddArea'
+
+const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Fazer aquilo lá', done: false},
+    {id: 2, name: 'Fazer aquilo lá', done: true}
+  ])
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list]
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+
+    setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
+  return(
+    <Style.Container>
+      <Style.Area>
+        <Style.Header>Lista de Tarefas</Style.Header>    
+        <AddArea onEnter={handleAddTask}/>
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} onChange={handleTaskChange}/>
+        ))}
+      </Style.Area>
+    </Style.Container>
   );
 }
 
